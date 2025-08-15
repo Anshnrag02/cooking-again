944c7ce4b36fc5fbac76652429bb29a2ab097efb
Ansh Anurag <anxhnurag@gmail.com>
2025-08-15 10:41:37 +0000

    day1

--- bitman/sumofpow3.cpp
+++ bitman/sumofpow3.cpp
@@ -9,3 +9,19 @@ public:
         return n==0;
     }
 };
+
+
+// more optimal solution 
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        int k = pow(3,16);
+        for (int i = 16; i>=0; --i){
+            if(n>=k) {
+                n-=k;
+            }
+            k/=3;
+        }
+        return n==0;
+    }
+};
