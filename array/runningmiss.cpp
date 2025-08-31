f0b0f789b8a05c335dccc9aa721c02450ea29689
Ansh Anurag <anxhnurag@gmail.com>
2025-08-31 18:46:22 +0000

    day 7

--- array/runningmiss.cpp
+++ array/runningmiss.cpp
@@ -24,3 +24,18 @@ public:
         return n;
     }
 };
+
+
+// better solution 
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int xorr = n;
+        for (int i = 0; i < n; i++) {
+            xorr ^= i ^ nums[i];
+        }
+        return xorr;
+    }
+};
