eaa8deb5aaa27f0788ecc50469f227bdbf588408
Ansh Anurag <anxhnurag@gmail.com>
2026-03-03 16:33:02 +0000

    changes

--- 2D DP/uniquePaths.cpp
+++ 2D DP/uniquePaths.cpp
@@ -11,4 +11,18 @@ public:
     }
 };
 
-
+// optimizing memory
+class Solution {
+public:
+    // 1 1 1 1 1 1
+    // 1 1 1 1 1 1
+    int uniquePaths(int n, int m) {
+        vector<vector<int>>dp(2,vector<int>(m,1));
+        for(int i = 1 ; i < n ; ++i){
+            for(int j = 1; j<m ; ++j){
+                dp[i%2][j] = dp[(i-1)%2][j] + dp[i%2][j-1];
+            }
+        }
+        return dp[(n-1)%2][m-1];
+    }
+};
