cab4d124e9d9114f597fa977f2a5a432d1d89f18
Ansh Anurag <anxhnurag@gmail.com>
2026-03-07 09:28:11 +0000

    cook3

---
+++ 1D DP/rob.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rob(vector<int>& a) {
+        int n = a.size();
+        if(n==1) return a[0];
+        if(n==2) return max(a[0],a[1]);
+        for(int i = 2 ; i < n ; ++i){
+            a[i]+=max(a[i-2],a[i-3]);
+        }
+        return max(a[n-1],a[n-2]);
+    }
+};
