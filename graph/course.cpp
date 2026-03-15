6ca7f941e4b19b6fed7cb3f29d211bbed48035bd
Ansh Anurag <anxhnurag@gmail.com>
2026-03-15 14:12:40 +0000

    cooks

---
+++ graph/course.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    bool canFinish(int V, vector<vector<int>>& a) {
+        vector<int>ans;
+        queue<int>q;
+        vector<int>indegree(V,0);
+        vector<vector<int>> adj(V);
+        for(vector<int> v: a){
+            adj[v[1]].push_back(v[0]);
+            ++indegree[v[0]];
+        }
+        for(int i = 0 ; i < V ; ++i){
+            if(!indegree[i]) q.push(i);
+        }
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(int x: adj[node]){
+                if(indegree[x]){
+                    --indegree[x];
+                    if(!indegree[x]){
+                        q.push(x);
+                    }
+                }
+            }
+        }
+
+        if(ans.size() < V) return false;
+        
+        return true;
+    }
+};
